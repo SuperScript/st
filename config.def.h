bf0db60efa891533b1b9cb7d67fa790406801735
William Erik Baxter <web@superscript.com>
2024-04-28 10:05:21 -0500

    Use Semibold

--- config.def.h
+++ config.def.h
@@ -6,7 +6,7 @@
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
 /* static char *font = "Iosevka Nerd Font Mono:size=20:antialias=true:autohint=true";*/
-static char *font = "SauceCodePro Nerd Font Mono:size=16:antialias=true:autohint=true";
+static char *font = "SauceCodePro Nerd Font Mono:size=16:antialias=true:autohint=true:style=Semibold";
 static int borderpx = 2;
 
 /*
