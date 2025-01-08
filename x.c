3cf17b3fcb380eb2f6e929bfc9dd2673dad6dece
William Erik Baxter <web@superscript.com>
2025-01-07 21:49:01 -0600

    Add option to toggle colors at launch

--- x.c
+++ x.c
@@ -290,9 +290,15 @@ numlock(const Arg *dummy)
 }
 
 void
-swapcolors(const Arg *dummy)
+togglecolors(const Arg *dummy)
 {
 	usealtcolors = !usealtcolors;
+}
+
+void
+swapcolors(const Arg *dummy)
+{
+	togglecolors(dummy);
 	xloadcols();
 	redraw();
 }
@@ -1988,6 +1994,9 @@ main(int argc, char *argv[])
 		break;
 	case 'n':
 		opt_name = EARGF(usage());
+		break;
+	case 's':
+		togglecolors(0);
 		break;
 	case 't':
 	case 'T':
